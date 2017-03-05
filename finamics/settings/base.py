d10732ab73479a456f67e39cc6cdb693bca3358e
JackLaw <Jack.Law@coherent.com.hk>
2017-03-05 22:57:35 +0800

    Change for staticfiles

--- finamics/settings/base.py
+++ finamics/settings/base.py
@@ -120,11 +120,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
-STATIC_URL = '/static/'
 
 STATIC_HOST = os.environ.get('DJANGO_STATIC_HOST', '')
 
--- finamics/urls.py
+++ finamics/urls.py
@@ -1,8 +1,10 @@
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('dashboard.urls')),
     url(r'^api', include('instrument.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
